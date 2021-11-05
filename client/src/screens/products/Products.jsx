231a64a87464b2af8c6c4c43937ee5d949c45ccb
mattmoyka <mmoyka17@gmail.com>
2021-11-05 11:02:10 -0400

    more product list css

--- client/src/screens/products/Products.jsx
+++ client/src/screens/products/Products.jsx
@@ -7,23 +7,34 @@ export default function Products(props) {
 
 
   return (
-    <div>
+    <div className="product-page">
       <div className="product-list-header">
         <h3>Product List</h3>
-        <Link to='/products/create'>Add Product</Link>
+        <Link to='/products/create' id='product-link'>Add Product</Link>
       </div>
-      {products.map((product) => (
-        <Link to={`/products/${product.id}`} className='product-containers' id='product-link' key={product.id}>
-          {/* <div className='product-containers'> */}
-          <div>{product.name}</div>
-          <div>${product.cost + product.profit}</div>
-          <div>${product.cost}</div>
-          <div>${product.profit}</div>
-          <div>{Math.round(product.profit / (product.cost + product.profit) * 100)}%</div>
-          <div>{product.updated_at}</div>
-          {/* </div> */}
-        </Link>
-      ))}
+      <div className='product-containers-title'>
+
+        <div>Name</div>
+        <div>Total</div>
+        <div>Cost</div>
+        <div>Profit</div>
+        <div>Margin</div>
+        <div>Updated</div>
+
+      </div>
+      <div className='product-list-container'>
+        {products.map((product) => (
+          <Link to={`/products/${product.id}`} className='product-containers' id='product-link' key={product.id}>
+            {/* <div className='product-containers'> */}
+            <div>{product.name}</div>
+            <div>${product.cost + product.profit}</div>
+            <div>${product.cost}</div>
+            <div>${product.profit}</div>
+            <div>{Math.round(product.profit / (product.cost + product.profit) * 100)}%</div>
+            <div>{product.updated_at}</div>
+            {/* </div> */}
+          </Link>
+        ))}</div>
     </div>
   )
 }
